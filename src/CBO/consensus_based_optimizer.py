61efc51246aa085e547d333dab19cd057ec5e629
Igor Tukh <igor-tukh@yandex.ru>
2022-05-02 09:01:33 +0300

    cbo: intermediate commit

--- src/CBO/consensus_based_optimizer.py
+++ src/CBO/consensus_based_optimizer.py
@@ -1,6 +1,9 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
+
+from src.CBO.distributions import NumpyNormal
+
 tfd = tfp.distributions
 
 from tensorflow.keras.optimizers import Optimizer
@@ -18,16 +21,18 @@ class CBO(Optimizer):
         self._set_hyper('alpha', alpha)
         self._V = initial_particles
         self._anisotropic = anisotropic
-        self._noise = tfd.Normal(loc=0., scale=1.)
+        # self._noise = tfd.Normal(loc=0., scale=1.)
+        self._noise = NumpyNormal()
         self._n_batches = 1 if n_batches is None else n_batches
         self.update_all_particles = update_all_particles
+        self._old_consensus = tf.zeros_like(self._V[0])
 
     def apply_cooling(self, epoch):
         self._set_hyper('alpha', self._get_hyper('alpha') * 2)
         self._sigma = self._sigma * np.log2(epoch + 1) / np.log2(epoch + 2)
 
-    def minimizer(self):
-        return self._compute_consensus_point()
+    def minimizer(self, objective=None):
+        return self._compute_consensus_point(objective=objective)
 
     def particles(self):
         return self._V
@@ -35,11 +40,12 @@ class CBO(Optimizer):
     def update_objective(self, objective):
         self._objective = objective
 
-    def _compute_consensus_point(self, batch=None):
+    def _compute_consensus_point(self, batch=None, objective=None):
         if batch is not None:
             batch = list(batch.reshape(-1))
+        objective = self._objective if objective is None else objective
         V = self._V if batch is None else tf.gather(self._V, indices=batch)
-        values = np.array([self._objective(v) for v in V])
+        values = np.array([objective(v) for v in V])
         weights = np.exp(-self._get_hyper('alpha') * (values - values.min())).reshape(-1, 1)
         consensus = tf.reshape(tf.reduce_sum(V * weights, axis=0) / weights.sum(), (1, -1))
         return consensus
@@ -49,7 +55,6 @@ class CBO(Optimizer):
             batches = np.arange(self._V.shape[0]).reshape(1, -1)
         else:
             batches = np.array_split(np.random.permutation(self._V.shape[0]), self._n_batches)
-        old_consensus = tf.zeros_like(self._V[0])
         for batch in batches:
             V_alpha = self._compute_consensus_point(batch.reshape(-1, 1))
 
@@ -69,9 +74,11 @@ class CBO(Optimizer):
                 self._V += tf.scatter_nd(indices=tf.constant(batch.reshape(-1, 1)), updates=V_update,
                                          shape=self._V.shape)
 
-            if np.less(tf.norm(old_consensus - tf.reshape(V_alpha, -1), ord=2, axis=0), 1e-5):
+            if np.less(tf.norm(self._old_consensus - tf.reshape(V_alpha, -1), ord=2, axis=0), 1e-5):
                 self._V += self._sigma * (self._dt ** 0.5) * self._noise.sample(self._V.shape)
-            old_consensus = tf.reshape(V_alpha, -1)
+            self._old_consensus = tf.reshape(V_alpha, -1)
+
+            self._compute_consensus_point()  # !!
 
     def _resource_apply_dense(self, grad, var, apply_state=None):
         self._step()
--- src/CBO/distributions.py
+++ src/CBO/distributions.py
@@ -1,7 +1,14 @@
 import tensorflow_probability as tfp
+import numpy as np
+import tensorflow as tf
 
 tfd = tfp.distributions
 
 
 def Normal(loc=0., scale=1.):
     return tfd.Normal(loc, scale)
+
+
+class NumpyNormal:
+    def sample(self, size):
+        return tf.convert_to_tensor(np.random.normal(0, 1, size))
